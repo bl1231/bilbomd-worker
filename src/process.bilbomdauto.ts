9413a060e64314d4e663134ccb179c9da9a75035
Scott Classen <sclassen@lbl.gov>
2023-10-13 16:00:10 -0700

    refactor and add PAE and AutoRg to Auto jobs

--- src/process.bilbomdauto.ts
+++ src/process.bilbomdauto.ts
@@ -1,5 +1,5 @@
 import { Job as BullMQJob } from 'bullmq'
-import { IBilboMDAutoJob, BilboMDAutoJob } from './model/Job'
+import { BilboMDAutoJob, IBilboMDAutoJob } from './model/Job'
 import { User } from './model/User'
 import { sendJobCompleteEmail } from './mailer'
 import {
@@ -8,89 +8,129 @@ import {
   runMolecularDynamics,
   runFoxs,
   runMultiFoxs,
-  gatherResults
-} from './bilbomd'
+  gatherResults,
+  runPaeToConst,
+  runAutoRg
+} from './bilbomd.functions'
 
 const bilbomdUrl: string = process.env.BILBOMD_URL ?? 'https://bilbomd.bl1231.als.lbl.gov'
 
-const initializeJob = async (job: IBilboMDAutoJob) => {
-  // Make sure the user exists
-  const foundUser = await User.findById(job.user).lean().exec()
+const initializeJob = async (MQJob: BullMQJob, DBjob: IBilboMDAutoJob) => {
+  // Make sure the user exists in MongoDB
+  const foundUser = await User.findById(DBjob.user).lean().exec()
   if (!foundUser) {
-    throw new Error(`No user found for: ${job.uuid}`)
+    throw new Error(`No user found for: ${DBjob.uuid}`)
   }
-  // Set job status to Running
-  job.status = 'Running'
-  // await job.updateProgress(10)
+  // Clear the BullMQ Job logs
+  await MQJob.clearLogs()
+  // Set MongoDB status to Running
+  DBjob.status = 'Running'
   const now = new Date()
-  job.time_started = now
-  await job.save()
+  DBjob.time_started = now
+  await DBjob.save()
 }
 
-const cleanupJob = async (job: BullMQJob, DBJob: IBilboMDAutoJob) => {
+const cleanupJob = async (MQjob: BullMQJob, DBJob: IBilboMDAutoJob) => {
   DBJob.status = 'Completed'
   DBJob.time_completed = new Date()
   await DBJob.save()
-  // Send email to user
   sendJobCompleteEmail(DBJob.user.email, bilbomdUrl, DBJob.id, DBJob.title)
   console.log(`email notification sent to ${DBJob.user.email}`)
-  await job.log(`email notification sent to ${DBJob.user.email}`)
+  await MQjob.log(`email notification sent to ${DBJob.user.email}`)
 }
 
-const processBilboMDAutoJob = async (job: BullMQJob) => {
-  // console.log('BullMQ job:', job.data)
-  // console.log('BullMQ ID:', job.id)
-  await job.updateProgress(5)
+const processBilboMDAutoJobTest = async (MQjob: BullMQJob) => {
+  await MQjob.updateProgress(1)
 
-  const foundJob = await BilboMDAutoJob.findOne({ _id: job.data.jobid }).exec()
+  const foundJob = await BilboMDAutoJob.findOne({ _id: MQjob.data.jobid })
+    .populate({
+      path: 'user',
+      select: 'email'
+    })
+    .exec()
   if (!foundJob) {
-    throw new Error(`No job found for: ${job.data.jobid}`)
+    throw new Error(`No job found for: ${MQjob.data.jobid}`)
   }
+  await MQjob.updateProgress(5)
 
-  await initializeJob(foundJob)
-  await job.updateProgress(10)
+  // Initialize
+  await initializeJob(MQjob, foundJob)
+
+  // Use PAE to construct const.inp file
+  await runPaeToConst(foundJob)
+
+  // Use BioXTAS to calculate Rg_min and Rg_max
+  await runAutoRg(foundJob)
+
+  // More steps that require foundJob or updatedJob
+
+  await cleanupJob(MQjob, foundJob)
+  await MQjob.updateProgress(100)
+}
+
+const processBilboMDAutoJob = async (MQjob: BullMQJob) => {
+  await MQjob.updateProgress(1)
+
+  const foundJob = await BilboMDAutoJob.findOne({ _id: MQjob.data.jobid })
+    .populate({
+      path: 'user',
+      select: 'email'
+    })
+    .exec()
+  if (!foundJob) {
+    throw new Error(`No job found for: ${MQjob.data.jobid}`)
+  }
+  await MQjob.updateProgress(5)
+
+  // Initialize
+  await initializeJob(MQjob, foundJob)
+  await MQjob.updateProgress(10)
+
+  // Use PAE to construct const.inp file
+  await runPaeToConst(foundJob)
+
+  // Use BioXTAS to calculate Rg_min and Rg_max
+  await runAutoRg(foundJob)
 
   // CHARMM minimization
-  await job.log('start minimization')
-  await runMinimize(job, foundJob)
-  await job.log('end minimization')
-  await job.updateProgress(25)
+  await MQjob.log('start minimization')
+  await runMinimize(MQjob, foundJob)
+  await MQjob.log('end minimization')
+  await MQjob.updateProgress(25)
 
   // CHARMM heating
-  await job.log('start heating')
-  console.log('HEATING foundJob: ', foundJob)
-  await runHeat(job, foundJob)
-  await job.log('end heating')
-  await job.updateProgress(40)
+  await MQjob.log('start heating')
+  await runHeat(MQjob, foundJob)
+  await MQjob.log('end heating')
+  await MQjob.updateProgress(40)
 
   // CHARMM molecular dynamics
-  await job.log('start molecular dynamics')
-  console.log('MD foundJob: ', foundJob)
-  await runMolecularDynamics(job, foundJob)
-  await job.log('end molecular dynamics')
-  await job.updateProgress(60)
+  await MQjob.log('start molecular dynamics')
+  await runMolecularDynamics(MQjob, foundJob)
+  await MQjob.log('end molecular dynamics')
+  await MQjob.updateProgress(60)
 
   // Calculate FoXS profiles
-  await job.log('start FoXS')
-  await runFoxs(job, foundJob)
-  await job.log('end FoXS')
-  await job.updateProgress(80)
+  await MQjob.log('start FoXS')
+  await runFoxs(MQjob, foundJob)
+  await MQjob.log('end FoXS')
+  await MQjob.updateProgress(80)
 
   // MultiFoXS
-  await job.log('start MultiFoXS')
-  await runMultiFoxs(job, foundJob)
-  await job.log('end MultiFoXS')
-  await job.updateProgress(95)
+  await MQjob.log('start MultiFoXS')
+  await runMultiFoxs(MQjob, foundJob)
+  await MQjob.log('end MultiFoXS')
+  await MQjob.updateProgress(95)
 
-  // Prepare results for user
-  await job.log('start gather results')
-  await gatherResults(job, foundJob)
-  await job.log('end gather results')
-  await job.updateProgress(99)
+  // Prepare results
+  await MQjob.log('start gather results')
+  await gatherResults(MQjob, foundJob)
+  await MQjob.log('end gather results')
+  await MQjob.updateProgress(99)
 
-  await cleanupJob(job, foundJob)
-  await job.updateProgress(100)
-  return 'ok'
+  // Cleanup & send email
+  await cleanupJob(MQjob, foundJob)
+  await MQjob.updateProgress(100)
 }
 
-export { processBilboMDAutoJob }
+export { processBilboMDAutoJob, processBilboMDAutoJobTest }
