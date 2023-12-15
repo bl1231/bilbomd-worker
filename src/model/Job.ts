adc9fe7aea0dd896182ca3368b3724793b7f8649
Scott Classen <sclassen@lbl.gov>
2023-12-15 10:27:09 -0800

    copy schema from bilbomd-backend

--- src/model/Job.ts
+++ src/model/Job.ts
@@ -5,24 +5,28 @@ interface IJob extends Document {
   title: string
   uuid: string
   status: string
-  psf_file: string
-  crd_file: string
   data_file: string
   time_submitted: Date
-  time_started: Date
-  time_completed: Date
+  time_started?: Date
+  time_completed?: Date
   user: IUser
 }
 
 interface IBilboMDJob extends IJob {
+  psf_file: string
+  crd_file: string
   const_inp_file: string
   conformational_sampling: number
   rg_min: number
   rg_max: number
 }
 
-interface IBilboMDAutoJob extends IBilboMDJob {
+interface IBilboMDAutoJob extends IJob {
   pae_file: string
+}
+
+interface IBilboMDScoperJob extends IJob {
+  pdb_file: string
 }
 
 const jobSchema = new Schema<IJob>(
@@ -32,8 +36,6 @@ const jobSchema = new Schema<IJob>(
       required: true
     },
     uuid: { type: String, required: true },
-    psf_file: { type: String, required: true },
-    crd_file: { type: String, required: true },
     data_file: { type: String, required: true },
     status: {
       type: String,
@@ -41,8 +43,8 @@ const jobSchema = new Schema<IJob>(
       default: 'Submitted'
     },
     time_submitted: { type: Date, default: () => new Date(Date.now()) },
-    time_started: { type: Date },
-    time_completed: { type: Date },
+    time_started: Date,
+    time_completed: Date,
     user: {
       type: Schema.Types.ObjectId,
       ref: 'User',
@@ -54,87 +56,39 @@ const jobSchema = new Schema<IJob>(
   }
 )
 
-const bilboMdJobSchema = new Schema<IBilboMDJob>(
-  {
-    title: {
-      type: String,
-      required: true
-    },
-    uuid: { type: String, required: true },
-    psf_file: { type: String, required: true },
-    crd_file: { type: String, required: true },
-    const_inp_file: { type: String },
-    data_file: { type: String, required: true },
-    conformational_sampling: {
-      type: Number,
-      enum: [1, 2, 3, 4],
-      default: 1
-    },
-    rg_min: { type: Number, required: true, minimum: 10, maximum: 100 },
-    rg_max: { type: Number, required: true, minimum: 10, maximum: 100 },
-    status: {
-      type: String,
-      enum: ['Submitted', 'Pending', 'Running', 'Completed', 'Error'],
-      default: 'Submitted'
-    },
-    time_submitted: { type: Date, default: () => new Date(Date.now()) },
-    time_started: { type: Date },
-    time_completed: { type: Date },
-    user: {
-      type: Schema.Types.ObjectId,
-      ref: 'User',
-      required: true
-    }
+const bilboMdJobSchema = new Schema<IBilboMDJob>({
+  psf_file: { type: String, required: true },
+  crd_file: { type: String, required: true },
+  const_inp_file: { type: String, required: true },
+  conformational_sampling: {
+    type: Number,
+    enum: [1, 2, 3, 4],
+    default: 1
   },
-  {
-    timestamps: true
-  }
-)
+  rg_min: { type: Number, required: true, min: 10, max: 100 },
+  rg_max: { type: Number, required: true, min: 10, max: 100 }
+})
 
-const bilboMdAutoJobSchema = new Schema<IBilboMDAutoJob>(
-  {
-    title: {
-      type: String,
-      required: true
-    },
-    uuid: { type: String, required: true },
-    psf_file: { type: String, required: true },
-    crd_file: { type: String, required: true },
-    pae_file: { type: String, required: true },
-    const_inp_file: { type: String },
-    data_file: { type: String, required: true },
-    conformational_sampling: {
-      type: Number,
-      enum: [1, 2, 3, 4],
-      default: 1
-    },
-    rg_min: { type: Number, required: false },
-    rg_max: { type: Number, required: false },
-    status: {
-      type: String,
-      enum: ['Submitted', 'Pending', 'Running', 'Completed', 'Error'],
-      default: 'Submitted'
-    },
-    time_submitted: { type: Date, default: () => new Date(Date.now()) },
-    time_started: { type: Date },
-    time_completed: { type: Date },
-    user: {
-      type: Schema.Types.ObjectId,
-      ref: 'User',
-      required: true
-    }
-  },
-  {
-    timestamps: true
-  }
-)
+const bilboMdAutoJobSchema = new Schema<IBilboMDAutoJob>({
+  pae_file: { type: String, required: true }
+})
 
-const Job = model<IJob>('Job', jobSchema, 'jobs')
-const BilboMDJob = model<IBilboMDJob>('BilboMDJob', bilboMdJobSchema, 'jobs')
-const BilboMDAutoJob = model<IBilboMDAutoJob>(
-  'BilboMDAutoJob',
-  bilboMdAutoJobSchema,
-  'jobs'
-)
+const bilboMdScoperJobSchema = new Schema<IBilboMDScoperJob>({
+  pdb_file: { type: String, required: true }
+})
 
-export { Job, IJob, BilboMDJob, IBilboMDJob, BilboMDAutoJob, IBilboMDAutoJob }
+const Job = model('Job', jobSchema)
+const BilboMdJob = Job.discriminator('BilboMd', bilboMdJobSchema)
+const BilboMdAutoJob = Job.discriminator('BilboMdAuto', bilboMdAutoJobSchema)
+const BilboMdScoperJob = Job.discriminator('BilboMdScoperJob', bilboMdScoperJobSchema)
+
+export {
+  Job,
+  IJob,
+  BilboMdJob,
+  IBilboMDJob,
+  BilboMdAutoJob,
+  IBilboMDAutoJob,
+  BilboMdScoperJob,
+  IBilboMDScoperJob
+}
