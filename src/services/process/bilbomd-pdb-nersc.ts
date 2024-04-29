267fc4f5770997771df122db982dc0d3f8667e68
Scott Classen <sclassen@lbl.gov>
2024-04-29 14:21:48 -0700

    rename to be consistant

---
+++ src/services/process/bilbomd-pdb-nersc.ts
@@ -0,0 +1,65 @@
+import { Job as BullMQJob } from 'bullmq'
+import { BilboMdPDBJob } from 'models/Job'
+import { logger } from 'helpers/loggers'
+import { ensureValidToken } from 'services/functions/nersc-sf-api-tokens'
+import { initializeJob, prepareResults, cleanupJob } from '../bilbomd.functions'
+import { submitJobToNersc } from 'services/functions/nersc-jobs'
+
+const processBilboMDJobNerscTest = async (MQjob: BullMQJob) => {
+  const foundJob = await BilboMdPDBJob.findOne({ _id: MQjob.data.jobid })
+    .populate({
+      path: 'user',
+      select: 'email'
+    })
+    .exec()
+  if (!foundJob) {
+    throw new Error(`No job found for: ${MQjob.data.jobid}`)
+  }
+
+  await initializeJob(MQjob, foundJob)
+  await cleanupJob(MQjob, foundJob)
+}
+
+const processBilboMDPDBJobNersc = async (MQjob: BullMQJob) => {
+  try {
+    const token = await ensureValidToken()
+    await MQjob.updateProgress(1)
+
+    const foundJob = await BilboMdPDBJob.findOne({ _id: MQjob.data.jobid })
+      .populate({
+        path: 'user',
+        select: 'email'
+      })
+      .exec()
+
+    if (!foundJob) {
+      throw new Error(`No job found for: ${MQjob.data.jobid}`)
+    }
+    await MQjob.updateProgress(5)
+
+    // Initialize
+    await initializeJob(MQjob, foundJob)
+    await MQjob.updateProgress(10)
+
+    // SF API
+    const results = await submitJobToNersc(token, foundJob.uuid)
+    logger.info(`NERSC job ${results}`)
+
+    // Poll for status
+
+    // Prepare results
+    await MQjob.log('start gather results')
+    await prepareResults(MQjob, foundJob)
+    await MQjob.log('end gather results')
+    await MQjob.updateProgress(99)
+
+    // Cleanup & send email
+    await cleanupJob(MQjob, foundJob)
+    await MQjob.updateProgress(100)
+  } catch (error) {
+    logger.error(`Failed to process job: ${MQjob.data.uuid}`)
+    throw error
+  }
+}
+
+export { processBilboMDPDBJobNersc, processBilboMDJobNerscTest }
