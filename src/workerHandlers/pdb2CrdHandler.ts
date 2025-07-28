94f5100a53ae94d6ff76338dd18435cd734698e5
Scott Classen <sclassen@lbl.gov>
2025-07-28 13:05:16 -0700

    remove commented code

--- src/workerHandlers/pdb2CrdHandler.ts
+++ src/workerHandlers/pdb2CrdHandler.ts
@@ -1,15 +1,12 @@
 import { Job } from 'bullmq'
 import { logger } from '../helpers/loggers.js'
-// import { config } from '../config/config.js'
 import { processPdb2CrdJob } from '../services/pipelines/pdb-to-crd.js'
-// import { processPdb2CrdJobNersc } from '../services/pipelines/pdb-to-crd-nersc.js'
 import { WorkerJob } from '../types/jobtypes.js'
 
 export const pdb2CrdHandler = async (job: Job<WorkerJob>) => {
   logger.info(`pdb2CrdHandler: ${JSON.stringify(job.data)}`)
   try {
     logger.info(`Start Pdb2Crd job: ${job.name}`)
-    // await (config.runOnNERSC ? processPdb2CrdJobNersc(job) : processPdb2CrdJob(job))
     await processPdb2CrdJob(job)
     logger.info(`Finished job: ${job.name}`)
   } catch (error) {
