a02cdffcb15a0e5209280250f246bcbfe92bf99b
Scott Classen <sclassen@lbl.gov>
2025-09-05 11:26:06 -0700

    4 spaces god dammit

--- scripts/nersc/run-multifoxs.py
+++ scripts/nersc/run-multifoxs.py
@@ -1,4 +1,3 @@
-import os
 import subprocess
 
 # Paths
@@ -8,13 +7,13 @@ prefix = '../openmm/md/'
 
 # Read and process lines
 with open(input_file, 'r') as f:
-	lines = [prefix + line.strip() for line in f if line.strip()]
+    lines = [prefix + line.strip() for line in f if line.strip()]
 
 # Write to output file
 with open(output_file, 'w') as f:
-	f.write('\n'.join(lines) + '\n')
+    f.write('\n'.join(lines) + '\n')
 
 # Run multi_foxs command
 cmd = ['multi_foxs', '-o', '../saxs-data.dat', './foxs_dat_files_for_multifoxs.txt']
 with open('multi_foxs.log', 'w') as log:
-	subprocess.run(cmd, stdout=log, stderr=subprocess.STDOUT)
+    subprocess.run(cmd, stdout=log, stderr=subprocess.STDOUT)
